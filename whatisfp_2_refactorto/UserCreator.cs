f459cfd2cf9429f24a897f4f7bf08950c9204ae6
Chris Jansson <chris.janssom@gmail.com>
2017-03-07 17:14:27 +0100

    More shitty code

--- whatisfp_2_refactorto/UserCreator.cs
+++ whatisfp_2_refactorto/UserCreator.cs
@@ -1,11 +1,34 @@
+using System;
 using System.Collections.Generic;
 
 namespace Users
 {
     public class UserCreator
     {
+        private const int MinimumUsernameLength = 5;
+
+        public UserCreator(Func<string, User> getUserByName)
+        {
+            Success = false;
+            Errors = new List<string>();
+        }
+
         public User Create(string username, string email)
         {
+            if (username.Length < MinimumUsernameLength)
+            {
+                Success = false;
+                Errors.Add("Username must be atleast 5 characters");
+                return null;
+            }
+
+            if (!email.Contains("@"))
+            {
+                Errors.Add("Email must contain a @");
+                return null;
+                Success = false;
+            }
+
             return new User
             {
                 Username = username,
@@ -13,8 +36,8 @@ namespace Users
             };
         }
 
-        public bool Success { get; set; }
+        public bool Success { get; private set; }
 
-        public List<string> Errors { get; set; }
+        public List<string> Errors { get; private set; }
     }
 }
