984674068d83eab847c416af6d80cb722f9a83ea
Chris Jansson <chris.janssom@gmail.com>
2017-03-08 14:53:02 +0100

    Added a few tests

--- whatisfp_2_refactorto/UserDatabase.cs
+++ whatisfp_2_refactorto/UserDatabase.cs
@@ -1,19 +1,30 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
 
 namespace Users
 {
-
     public class FakeDatabase : IDatabase
     {
+        private List<User> _users = new List<User>();
+
         public void Save(User user)
         {
             if (user == null)
                 throw new ArgumentNullException(nameof(user));
-        }
-    
-        public void AssertUserSaved(string username) 
-        {
 
+            _users.Add(user);
+        }
+
+        public void AssertUserSaved(string username)
+        {
+            Assert.Contains(username, _users.Select(x => x.Username));
+        }
+
+        public void AssertUserNotSaved(string username)
+        {
+            Assert.DoesNotContain(username, _users.Select(x => x.Username));
         }
     }
 }
---
+++ whatisfp_2_refactorto/UserServiceTests.cs
@@ -0,0 +1,96 @@
+using Users;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Tests
+{
+    public class UnitTest1
+    {
+        private UserService _userService;
+        private FakeDatabase _fakeDatabase;
+
+        public UnitTest1(ITestOutputHelper output)
+        {
+            _fakeDatabase = new FakeDatabase();
+            _userService = new UserService(l => output.WriteLine(l), _fakeDatabase);
+        }
+
+        [Fact]
+        public void Imports_user()
+        {
+            ImportUsers(User("username", "some@email.com"));
+
+            _fakeDatabase.AssertUserSaved("username");
+        }
+
+
+        [Theory]
+        [InlineData("user")]
+        public void Does_not_import_user_with_invalid_username(string username)
+        {
+            ImportUsers(User(username, "some@email.com"));
+
+            _fakeDatabase.AssertUserNotSaved(username);
+        }
+
+        [Theory]
+        [InlineData("email")]
+        public void Does_not_import_user_with_invalid_email(string email)
+        {
+            ImportUsers(User("username", email));
+
+            _fakeDatabase.AssertUserNotSaved("username");
+        }
+
+        #region multiuser
+
+        [Fact]
+        public void Imports_multiple_users()
+        {
+            ImportUsers(
+                User("username", "some@email.com"),
+                User("username2", "some@email2.com"));
+
+            _fakeDatabase.AssertUserSaved("username");
+            _fakeDatabase.AssertUserSaved("username2");
+        }
+
+        [Fact]
+        public void Imports_first_user_when_second_fails()
+        {
+            ImportUsers(
+                User("username", "some@email.com"),
+                User("user", "some@email.com"));
+
+            _fakeDatabase.AssertUserSaved("username");
+            _fakeDatabase.AssertUserNotSaved("user");
+        }
+
+        [Fact]
+        public void Imports_second_user_when_first_fails()
+        {
+            ImportUsers(
+                User("user", "some@email.com"),
+                User("username", "some@email.com"));
+
+            _fakeDatabase.AssertUserSaved("username");
+            _fakeDatabase.AssertUserNotSaved("user");
+        }
+
+        #endregion
+
+        private UserDto User(string username, string email)
+        {
+            return new UserDto
+            {
+                Username = username,
+                Email = email
+            };
+        }
+
+        private void ImportUsers(params UserDto[] users)
+        {
+            _userService.ImportUsers(users);
+        }
+    }
+}
